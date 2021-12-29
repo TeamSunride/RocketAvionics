049b7370996ab382c9cc9181dfb2b4450724049b
TomD53 <tomtomgaming101@gmail.com>
2021-12-29 23:27:12 +0000

    Revert "add continuous integration workflow to build library, examples, and run unit tests (FINGERS CROSSED!)"
    This reverts commit f0608c22dc3292ab10a3f2b1787a5ebf6a85be10.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,9 @@
 #include <Arduino.h>
-#include "../lib/event_detectors/ApogeeDetector.h"
-#include "VirtualBarometer.h"
-
-ApogeeDetector apogee = ApogeeDetector();
-
-const unsigned long SERIAL_BAUD_RATE = 2000000;
-
-VirtualBarometer barometer = VirtualBarometer();
-
-void VirtualSensor::setup() {
-    barometer.allowTesting();
-    apogee.setup();
-}
-
-// TODO: LOOK AT C++ CLASS TEMPLATING! https://www.geeksforgeeks.org/templates-cpp/
 
 void setup() {
-    Serial.begin(SERIAL_BAUD_RATE);
-
-    Wire.begin();
-
-    barometer.connect();
-
-    VirtualSensor::setup();
+  // put your setup code here, to run once:
 }
 
 void loop() {
-    float pressure = barometer.getPressure();
-    apogee.run(pressure);
+  // put your main code here, to run repeatedly:
 }
